33c16b36ef22d9ae2fc1d6973644c6cccbb926a8
Nicole <nicoleamdubois@hotmail.com>
2022-07-04 12:57:15 -0600

    added react dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Header />
-    {/* Tinder Cards */}
-    {/* Buttons below tinder cards */}
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Iam the homepage</h1>
+          </Route>
+        </Switch>
+        {/* Tinder Cards */}
+        {/* Buttons below tinder cards */}
 
-    {/* Chats screen */}
-      {/* Individual chat screen */}
+        {/* Chats screen */}
+        {/* Individual chat screen */}
+      </Router>
     </div>
   );
 }
