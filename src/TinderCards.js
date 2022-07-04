1f3177727015ea8b54eea2aec1533990bcee9feb
Nicole <nicoleamdubois@hotmail.com>
2022-07-04 14:13:33 -0600

    Added more style

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
 
 function TinderCards() {
-    const  [people, setPeople] = useState([]);
+  const [people, setPeople] = useState([
+    {
+      name: "Steve Jobs",
+      url: "https://images.networthlist.org/images/23/steve-jobs.jpg",
+    },
+    {
+      name: "Mark Zuckerberg",
+      url: "https://vz.cnwimg.com/wp-content/uploads/2020/08/mark.jpg",
+    },
+  ]);
 
   return (
     <div>
-      <hi>Tinder Cards</hi>
+      <h1>Tinder Cards</h1>
+
+      {people.map((person) => (
+        <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
+          <div
+            style={{ backgroundImage: `url(${person.url})` }}
+            className="card"
+          >
+            <h3>(person.name)</h3>
+          </div>
+        </TinderCard>
+      ))}
     </div>
   );
 }
