3e093e24a908ef1ede3304efbf25d1c69038797c
Nicole <nicoleamdubois@hotmail.com>
2022-07-04 13:10:33 -0600

    added tinder card fuction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
             <h1>I am the chat page</h1>
           </Route>
           <Route path="/">
-            <h1>Iam the homepage</h1>
+            <TinderCards />
           </Route>
         </Switch>
         {/* Tinder Cards */}
---
+++ src/TinderCards.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function TinderCards() {
+    const  [people, setPeople] = useState([]);
+
+  return (
+    <div>
+      <hi>Tinder Cards</hi>
+    </div>
+  );
+}
+
+export default TinderCards;
