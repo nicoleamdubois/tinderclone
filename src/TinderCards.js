ea2c80332298422be90abe951b30eb76c334a21a
Nicole <nicoleamdubois@hotmail.com>
2022-07-04 18:49:03 -0600

    added style

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -17,17 +17,22 @@ function TinderCards() {
   return (
     <div>
       <h1>Tinder Cards</h1>
-
-      {people.map((person) => (
-        <TinderCard className="swipe" key={person.name} preventSwipe={['up', 'down']}>
-          <div
-            style={{ backgroundImage: `url(${person.url})` }}
-            className="card"
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key= {person.name}
+            preventSwipe={["up", "down"]}
           >
-            <h3>(person.name)</h3>
-          </div>
-        </TinderCard>
-      ))}
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>(person.name)</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 }
